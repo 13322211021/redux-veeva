af8e54a21b81b1519ddfdc2fd35e96b9bdab79e1
13322211021 <55481825+13322211021@users.noreply.github.com>
2021-07-07 11:07:45 +0800

    add schedule items

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,18 +1,58 @@
-import React, { Component } from 'react';
-// import './AppBody.css'
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Schedule extends Component {
+  getScheduleFirstLineItems() {
+    const firstLineItems = this.props.schedule.firstLineItems.map(
+      (item, index) => {
+        return (
+          <span
+            className="schedule-item"
+            style={{ paddingLeft: item.position + "px" }}
+          >
+            <div className="icon"></div>
+          </span>
+        );
+      }
+    );
 
+    return firstLineItems;
+  }
+
+  getScheduleSecondLineItems() {
+    const secondLineItems = this.props.schedule.secondLineItems.map(
+      (item, index) => {
+        return (
+          <span
+            className="schedule-item"
+            style={{ paddingLeft: item.position + "px" }}
+          >
+            <div className={"icon " + item.eventType}></div>
+          </span>
+        );
+      }
+    );
+
+    return secondLineItems;
+  }
 
   render() {
     const component = (
       <div className="schedule-component">
+        <div className="schedule-first-line">
+          {this.getScheduleFirstLineItems()}
+        </div>
+        <div className="schedule-second-line">
+          {this.getScheduleSecondLineItems()}
+        </div>
       </div>
-      );
+    );
     return component;
   }
 }
 
-export default Schedule;
+Schedule.propTypes = {
+  schedule: PropTypes.object
+};
 
+export default Schedule;
--- src/components/mock/ALL_DATA.js
+++ src/components/mock/ALL_DATA.js
@@ -1,51 +1,51 @@
-import * as EventType from './../../style/constant/EventType';
+import * as EventType from "./../../style/constant/EventType";
 
 var MOCK_DATA = [
-    {
-      id: 1,
-      hcp: {
-        aceName: 'ACE 1',
-        doctorName: 'Dr Jack Smith',
-        hospitalName: 'Hostpital name'
-      },
-      schedule: {
-        firstLine: [
-          {eventType: EventType.MAIL, hasDone: false, time: 34},
-          {eventType: EventType.MEETING, hasDone: true, time: 92},
-          {eventType: EventType.TELEPHONE, hasDone: false, time: 140},
-          {eventType: EventType.MAIL, hasDone: false, time: 190},
-          {eventType: EventType.REMOTE, hasDone: false, time: 232},
-          {eventType: EventType.MAIL, hasDone: false, time: 270},
-          {eventType: EventType.MEETING, hasDone: false, time: 301},
-          {eventType: EventType.MAIL, hasDone: false, time: 348},
-          {eventType: EventType.MEETING, hasDone: false, time: 390},
-          {eventType: EventType.REMOTE, hasDone: false, time: 467},
-          {eventType: EventType.MEETING, hasDone: false, time: 527},
-          {eventType: EventType.REMOTE, hasDone: true, time: 575},
-          {eventType: EventType.MEETING, hasDone: true, time: 620},
-          {eventType: EventType.TELEPHONE, hasDone: true, time: 700},
-          {eventType: EventType.MEETING, hasDone: true, time: 757},
-          {eventType: EventType.MEETING, hasDone: true, time: 813},
-        ],
-        secondLine: [
-          {eventType: EventType.MAIL, time: 10},
-          {eventType: EventType.MEETING, time: 10},
-          {eventType: EventType.TELEPHONE, time: 10},
-          {eventType: EventType.MAIL, time: 10},
-          {eventType: EventType.REMOTE, time: 10},
-          {eventType: EventType.MEETING, time: 10},
-          {eventType: EventType.MAIL, time: 10},
-          {eventType: EventType.MAIL, time: 10},
-          {eventType: EventType.MAIL, time: 10},
-        ]
-      },
-      frequency: {
-        target: [6,3,6,5,2,0],
-        todo: [2,2,1,1,1,0],
-        calandar: [2,0,4,1,0,0],
-        done: [2,1,1,2,0,0]
-      }
+  {
+    id: 1,
+    hcp: {
+      aceName: "ACE 1",
+      doctorName: "Dr Jack Smith",
+      hospitalName: "Hostpital name"
+    },
+    schedule: {
+      firstLineItems: [
+        { eventType: EventType.MAIL, hasDone: false, position: 34 },
+        { eventType: EventType.MEETING, hasDone: true, position: 20 },
+        { eventType: EventType.TELEPHONE, hasDone: false, position: 16 },
+        { eventType: EventType.MAIL, hasDone: false, position: 14 },
+        { eventType: EventType.REMOTE, hasDone: false, position: 10 },
+        { eventType: EventType.MAIL, hasDone: false, position: 2 },
+        { eventType: EventType.MEETING, hasDone: false, position: 0 },
+        { eventType: EventType.MAIL, hasDone: false, position: 12 },
+        { eventType: EventType.MEETING, hasDone: false, position: 10 },
+        { eventType: EventType.REMOTE, hasDone: false, position: 56 },
+        { eventType: EventType.MEETING, hasDone: false, position: 45 },
+        { eventType: EventType.REMOTE, hasDone: true, position: 21 },
+        { eventType: EventType.MEETING, hasDone: true, position: 18 },
+        { eventType: EventType.TELEPHONE, hasDone: true, position: 65 },
+        { eventType: EventType.MEETING, hasDone: true, position: 20 },
+        { eventType: EventType.MEETING, hasDone: true, position: 30 }
+      ],
+      secondLineItems: [
+        { eventType: EventType.MAIL, position: 10 },
+        { eventType: EventType.MEETING, position: 10 },
+        { eventType: EventType.TELEPHONE, position: 10 },
+        { eventType: EventType.MAIL, position: 10 },
+        { eventType: EventType.REMOTE, position: 10 },
+        { eventType: EventType.MEETING, position: 10 },
+        { eventType: EventType.MAIL, position: 10 },
+        { eventType: EventType.MAIL, position: 10 },
+        { eventType: EventType.MAIL, position: 10 }
+      ]
+    },
+    frequency: {
+      target: [6, 3, 6, 5, 2, 0],
+      todo: [2, 2, 1, 1, 1, 0],
+      calandar: [2, 0, 4, 1, 0, 0],
+      done: [2, 1, 1, 2, 0, 0]
     }
-  ];
+  }
+];
 
 export default MOCK_DATA;
